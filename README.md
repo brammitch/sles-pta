85df54836b598240c86ae7afc6de314232f8c17f
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2021-10-13 07:55:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## SLES-PTA
+
+- Simple project to pick raffle winners from provided CSV input
+- Uses Typescript and ESM with ts-node
+
+### Installation
+
+```
+npm i
+```
+
+### Usage
+
+```
+npm run ts-node ./src/spooky-bingo-2021.ts
+```
+
+or
+
+```
+npm run tsc
+node ./src/spooky-bingo-2021.js
+```
---
+++ src/spooky-bingo-2021.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import path from "path";
+import csv from "fast-csv";
+import pickRandom from "pick-random";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+interface CsvData {
+  student_first_name: string;
+  student_last_name: string;
+  email: string;
+  one_ticket: string;
+  ten_ticket: string;
+  twenty_ticket: string;
+  total_tickets: string;
+}
+
+interface FormattedCsvData {
+  student_name: string;
+  email: string;
+  tickets: number;
+}
+
+const data: FormattedCsvData[] = [];
+const entries: string[] = [];
+
+fs.createReadStream(path.resolve(__dirname, "assets", "raffle-test.csv"))
+  .pipe(csv.parse({ headers: true }))
+  .on("error", (error) => console.error(error))
+  .on("data", (row: CsvData) =>
+    data.push({
+      student_name: `${row.student_first_name} ${row.student_last_name}`,
+      email: row.email,
+      tickets: parseInt(row.total_tickets),
+    })
+  )
+  .on("end", () => {
+    data.forEach((d) => {
+      for (let i = 1; i <= d.tickets; i++) {
+        entries.push(`${d.student_name} ${d.email}`);
+      }
+    });
+
+    const winners = pickRandom(entries, { count: 7 });
+    console.log("winners", winners);
+  });
